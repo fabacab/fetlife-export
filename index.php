834ab88dc4d83000669d8396dcc613b4e3d9c5f1
Meitar Moscovitz <meitarm@gmail.com>
2012-08-09 02:56:45 -0500

    Add `<meta>` element explicitly indicating character set is UTF-8.

--- index.php
+++ index.php
@@ -51,6 +51,7 @@ if (!file_exists(FLEXPORT_ROBOTS_TXT)) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>FetLife Exporter</title>
 </head>
 <body>
